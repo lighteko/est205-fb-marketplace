1d49ab8c51e4a0d6cf9a32c5f0bf0aaec1bd140b
lighteko <dev.lighteko@gmail.com>
2024-11-12 13:49:17 -0500

    placeholding files

---
+++ client/src/components/common/CircButton.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CircButton(): JSX.Element {
+  return <></>;
+}
---
+++ client/src/views/FavoritesView.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FavoritesView(): JSX.Element {
+  return <></>;
+}
