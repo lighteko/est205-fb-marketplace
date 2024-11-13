8d152bbc63e17410b525d886870b0da3a2e31d3e
kgeonoh <gkim1146@gmail.com>
2024-11-13 14:59:52 -0500

    Camera and Post and Rate

--- client/src/shared/router.tsx
+++ client/src/shared/router.tsx
@@ -4,23 +4,25 @@ import CategoryView from "../views/CategoryView";
 import ChatroomView from "../views/ChatroomView";
 import ChatView from "../views/ChatView";
 import CreatePostView from "../views/CreatePostView";
+import RatingView from "../views/RatingView";
 import FavoritesView from "../views/FavoritesView";
 import MainView from "../views/MainView";
 import MyDealsView from "../views/MyDealsView";
 import MyProfileView from "../views/MyProfileView";
-import PostView from "../views/PostVIew";
+import PostView from "../views/PostView";
 import UserView from "../views/UserView";
 
 const Router = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainView />} />
+        <Route path="/" element={<CreatePostView />} />
         <Route path="/:category" element={<MainView />} />
         <Route path="/category" element={<CategoryView />} />
         <Route path="/chatroom/:chatroom/chat" element={<ChatView />} />
         <Route path="/chatrooms" element={<ChatroomView />} />
         <Route path="/post/create" element={<CreatePostView />} />
+        {/* <Route path="/post/rating" element={<RatingView />} /> */}
         <Route path="/post/:post" element={<PostView />} />
         <Route path="/favorites" element={<FavoritesView />} />
         <Route path="/mydeals" element={<MyDealsView />} />
