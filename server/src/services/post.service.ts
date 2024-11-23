5a21642dcecdba54db02875f1a11a71a3abc754b
lighteko <dev.lighteko@gmail.com>
2024-11-22 23:50:58 -0500

    integrate image service and post service

--- server/src/services/post.service.ts
+++ server/src/services/post.service.ts
@@ -1,8 +1,24 @@
-import PostDAO from '../daos/post.dao';
+import PostDAO from "../daos/post.dao";
+import ImageService from "./image.service";
+import PostPayload from "../types/post.payload";
 
 class PostService {
-  async createPost(postData: Object) {
-    return await PostDAO.createPost(postData);
+  async createPost(postData: PostPayload) {
+    const images = postData.images;
+    const imageIds = [];
+    try {
+      for (let image of images) {
+        const res = await ImageService.uploadImage(image);
+        imageIds.push(res._id);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    const post = {
+      ...postData,
+      images: imageIds,
+    };
+    return await PostDAO.createPost(post);
   }
 
   async getPosts() {
@@ -17,7 +33,7 @@ class PostService {
     return await PostDAO.getPostById(postId);
   }
 
-  async updatePost(postId: string, postData: Object) {
+  async updatePost(postId: string, postData: PostPayload) {
     return await PostDAO.updatePost(postId, postData);
   }
 
